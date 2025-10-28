ab587e75cf4ad1f3acb6e2f9a30dd11f2d2e0b2f
Zeqara <artembelii25@gmail.com>
2025-10-28 13:33:48 +0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -884,7 +884,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         } catch (error) {
             console.error("Ошибка загрузки инструкции для админа:", error);
-            container.innerHTML = '<p>Ошибка загрузки.</p>';
+            container.innerHTML = '<p>Роллан Черт хули смотришь.</p>';
         }
     }
 
